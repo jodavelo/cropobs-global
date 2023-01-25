ae74b46975e52e21122d6856ce23ad53fe00f1e5
José Daniel Vergara Lozano <j.d.vergara@cgiar.org>
2023-01-25 09:40:45 -0500

    static resources from external sources

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n
+  i18n,
+  images: {
+    domains: ['oeactest.ciat.cgiar.org', 'cassavalighthousetest.ciat.cgiar.org', 'commonbeanobservatorytst.ciat.cgiar.org', 'musaobservatorytst.ciat.cgiar.org']
+  }
 }
 
 module.exports = nextConfig
