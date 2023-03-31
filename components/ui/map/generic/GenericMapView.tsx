1676e4209d937b61275860ce63df41edcf05ad8d
José Daniel Vergara Lozano <j.d.vergara@cgiar.org>
2023-03-31 15:56:45 -0500

    map version 1 for databases section

---
+++ components/ui/map/generic/GenericMapView.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+
+interface Props {
+    divContainer: string;
+    geoJsonUrl: string;
+}
+
+export const GenericMapView = ({ divContainer, geoJsonUrl }: Props) => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2lhdGttIiwiYSI6ImNraGdmbDZjejAxNTMycXBwNXppeHIyYjkifQ.Ucfm2G0KapInAojq6f9BZw';
+    console.log(divContainer)
+    // Create a new map.
+    
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: divContainer,
+            // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+            style: 'mapbox://styles/ciatkm/ckhgfstwq018818o06dqero91',
+            zoom: 1,
+            center: [-10.707019, -61.616079]
+        });
+
+        map.on('load', () => {
+            // Add a source for the state polygons.
+            map.addSource('countries', {
+                'type': 'geojson',
+                'data': geoJsonUrl
+            });
+            
+            // Add a layer showing the state polygons.
+            map.addLayer({
+                    'id': 'states-layer',
+                    'type': 'fill',
+                    'source': 'countries',
+                    'paint': {
+                    'fill-color': 'rgba(200, 100, 240, 0.4)',
+                    'fill-outline-color': 'rgba(200, 100, 240, 1)'
+                }
+            });
+            
+            // When a click event occurs on a feature in the states layer,
+            // open a popup at the location of the click, with description
+            // HTML from the click event's properties.
+            map.on('click', 'states-layer', (e) => {
+                new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                // .setHTML(e.features[0].properties.name)
+                .setHTML( e!.features![0].properties!.esp_name! )
+                .addTo(map);
+            });
+            
+            // Change the cursor to a pointer when
+            // the mouse is over the states layer.
+            map.on('mouseenter', 'states-layer', () => {
+                map.getCanvas().style.cursor = 'pointer';
+            });
+            
+            // Change the cursor back to a pointer
+            // when it leaves the states layer.
+            map.on('mouseleave', 'states-layer', () => {
+                    map.getCanvas().style.cursor = '';
+                });
+            });
+    })
+    
+
+    
+
+    return (
+        <div 
+            id={ divContainer }
+            style={{ 
+                height: '100%',
+                width: '100%',
+            }}
+        ></div>
+    )
+}
--- pages/about/databases.tsx
+++ pages/about/databases.tsx
@@ -6,24 +6,34 @@ import { useTranslation } from 'react-i18next';
 import { Layout } from '../../components/layouts'
 import { Col, Container, Row } from 'react-bootstrap';
 
-import styles from './about.module.css';
+import styles from './databases.module.css';
 import { useWindowSize } from '../../hooks';
+import { GenericMapView } from '../../components/ui/map/generic';
 
 const DatabasesPage: NextPage = () => {
 
     const { t: dataTranslate } = useTranslation('about');
     return (
-        <Layout title="Data">
+        <Layout title="Databases">
             {/* <h1>{ dataTranslate('about-title') }</h1> */}
-            <Container className={ styles['general-containers'] }>
-                <Row className={ styles['general-containers'] }>
-                    <Col xs={ 12 } md={ 8 } className={ styles.description }>
-                        <h3 className={ styles['center-text'] }>Lorem ipsum</h3>
-                        <p className={ styles['justify-text'] }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in leo porttitor, laoreet dolor sit amet, iaculis purus. In hac habitasse platea dictumst. Nunc tempor sollicitudin nisi, et eleifend ligula pharetra quis. Vivamus rhoncus augue a nulla feugiat, ut accumsan eros sollicitudin. Suspendisse vulputate, turpis vel bibendum rutrum, dui felis maximus erat, at luctus nisl odio ut lectus. Mauris quis justo libero. Ut eleifend nec quam vel pellentesque. Vestibulum non tincidunt sem, sed eleifend nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In consectetur diam non sapien aliquet, ut cursus arcu dignissim. Vivamus iaculis ultrices mollis. Etiam molestie pharetra faucibus. Fusce non ornare orci. Etiam id orci quis urna ullamcorper viverra. Vivamus fringilla ipsum et tellus fermentum commodo.</p>
-                    </Col>
-                    <Col xs={ 12 } md={ 4 } className={ styles.credits }>
-                    <h3 className={ styles['center-text'] }>Lorem ipsum</h3>
-                        <p className={ styles['justify-text'] }>Duis sed ultrices dui, lobortis ullamcorper turpis. Duis interdum, quam at blandit dapibus, nisl lectus lobortis sapien, id volutpat ante libero ac purus. Nullam sit amet velit in erat pharetra posuere sit amet vitae mi. Aliquam lobortis sapien sed tortor feugiat, nec pellentesque libero dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pretium, libero non dignissim bibendum, neque erat sagittis tortor, at cursus odio metus eget arcu.</p>
+            <Container fluid>
+                <Row>
+                    <Col xs={ 12 }>
+                        <Row>
+                            <Col xs={ 12 } className={ styles['databases-section-description'] } >
+                                <div className={ styles['databases-section-description-content'] }>
+                                    <h2 className={ styles['title-description'] }>Title</h2>
+                                    <p className={ styles['description-center'] }>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde voluptatum ipsum in officiis! Iusto explicabo accusantium aperiam molestiae nihil odit eligendi sit itaque ipsam! Nesciunt soluta quibusdam voluptates necessitatibus?</p>
+                                    <p className={ styles['description-center'] }>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non unde voluptatum ipsum in officiis! Iusto explicabo accusantium aperiam molestiae nihil odit eligendi sit itaque ipsam! Nesciunt soluta quibusdam voluptates necessitatibus?</p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={ 12 } lg={ 7 } className={ styles['map-container'] }>
+                                <GenericMapView divContainer='databases-map' geoJsonUrl='https://oeactest.ciat.cgiar.org/api/v1/geojson/countries' />
+                            </Col>
+                            <Col xs={ 12 } lg={ 5 } className={ styles['grid-layout-cards'] }></Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
