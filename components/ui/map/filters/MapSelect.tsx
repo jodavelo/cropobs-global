6e638ff916dec6d422048b5b108685a9fae56678
eorozco <e.orozco@cgiar.org>
2023-04-05 10:52:55 -0500

    mapSelect original

--- components/ui/map/filters/MapSelect.tsx
+++ components/ui/map/filters/MapSelect.tsx
@@ -1,5 +1,4 @@
-import React, { FC, useContext } from 'react';
-import { LeftSideMenuContext } from '../../../../context/map/leftsidemenu';
+import React, { FC } from 'react';
 import { SelectOptions } from '../../../../interfaces/data';
 import styles from './mapSelect.module.css';
 
@@ -9,17 +8,11 @@ interface MapSelectInterface {
     setSelected: Function
     atrName: string
     id?: string
-    setShowGraphs: (data: boolean) => void;
-    setShowMap: (data: boolean) => void;
-    setMapCol: (data: number) => void;
-    setGraphsCol: (data: number) => void;
 }
 
-export const MapSelect: FC<MapSelectInterface> = ({ options={ values: [], names: []}, selected, setSelected, atrName, id=undefined , setShowGraphs, setShowMap, setMapCol, setGraphsCol }) => {
-    const { activeMapButton } = useContext( LeftSideMenuContext );
+export const MapSelect: FC<MapSelectInterface> = ({ options={ values: [], names: []}, selected, setSelected, atrName, id=undefined }) => {
     const { values, names } = options;
     const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        activeMapButton();
         setSelected( (prevState: Record<string, any>) => ({
             ...prevState,
             [atrName]: e.target.value
