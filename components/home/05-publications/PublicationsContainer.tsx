a8343577630813827d1c6a691d474484e9d484ca
Ricardo Pedreros <rpedreros>
2023-05-31 16:30:24 -0500

    fixing publs link

--- components/home/05-publications/PublicationsContainer.tsx
+++ components/home/05-publications/PublicationsContainer.tsx
@@ -4,58 +4,48 @@ import { v4 as uuidv4 } from 'uuid';
 import { GliderComponent } from '../../ui/glider';
 import { PublicationCard } from './';
 import styles from './publications.module.css';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const publications = [
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-    {
-        image_url: 'https://cgspace.cgiar.org/bitstream/handle/10568/121950/U22ArtJjagweRoleNothomDev.pdf.jpg?sequence=4&isAllowed=y',
-        title: 'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
-        link: 'https://cgspace.cgiar.org/'
-    },
-]
+const publ_text = {
+    title: 'Lastest Publications',
+    title_es: 'Publicaciones más recientes',
+    title_pt: 'Publicações mais recentes',
+}
+
+const config = {
+    url : process.env.NEXT_PUBLIC_JSON_URL + 'beans_publ',
+    headers: {
+    "api_key": process.env.NEXT_PUBLIC_JSON_API_KEY
+    }
+}
+
+const buildPubl = (rawPubl:any) => {
+    return {
+        title: rawPubl.title,
+        link: rawPubl.url,
+        image_url: rawPubl.thumbnail
+    }
+}
 
 export const PublicationsContainer = () => {
-
-    const publicationsCards = publications.map((publication, idx) => ( <PublicationCard key={ idx } publication={ publication }  /> ));
-
+    let [publ,setPubl] = useState([]);
+    useEffect(() => {
+        axios(config)
+        .then(response => {
+            setPubl(JSON.parse(response.data.json));
+        })
+        .catch(error => {
+            console.log(error)
+        })
+      }, [ ]);
+    const publicationsCards = publ.map((publication, idx) => ( <PublicationCard key={ idx } publication={ buildPubl(publication) }  /> ));
+    const { locale } = useRouter();
     return (
         <div className={styles['publications-container']}>
-            <h2 className={ styles['publications-section-title'] }>Publications</h2>
-            <GliderComponent key={ uuidv4() }  breakpoint={ 800 } items={ publicationsCards } slidesToShow={ 4 } />
+            <h2 className={ styles['publications-section-title'] }>{ locale == 'en' ? publ_text.title : ( locale == 'es' ? publ_text.title_es : publ_text.title_pt ) }</h2>
+            <GliderComponent key={ uuidv4() }  breakpoint={ 1000 } items={ publicationsCards } slidesToShow={ 4 } />
         </div>
     )
 }
