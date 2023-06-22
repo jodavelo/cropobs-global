cb4cc6df98a59be395a23ad60922937003e903ff
Ricardo Pedreros <rpedreros>
2023-06-22 08:27:36 -0500

    bugfixing in production value and coonsumption

--- pages/data/consumption.tsx
+++ pages/data/consumption.tsx
@@ -272,7 +272,7 @@ const DataPage: NextPage = () => {
         setTitleSection(dataTranslate('section-name')!);
         setChartTxts1Label(dataTranslate('chart1-title').replace('#{}', locationName2));
         setChartTxts2Label(dataTranslate('chart2-title').replace('#{}', locationName2))
-    }, [locale])
+    }, [locale, dataTranslate])
 
     useEffect(() => {
         if (elementsData && !isLoadingElements) {
