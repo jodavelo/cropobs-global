e398276fe8f78bb188eed16683d968d15e5af6d5
José Daniel Vergara Lozano <j.d.vergara@cgiar.org>
2023-06-29 14:24:27 -0500

    databases almost it's full

--- next.config.js
+++ next.config.js
@@ -7,7 +7,33 @@ const nextConfig = {
   swcMinify: true,
   i18n,
   images: {
-    domains: ['commonbeanobservatorytst.ciat.cgiar.org', 'www.fao.org', 'oeactest.ciat.cgiar.org']
+    domains: [
+      'commonbeanobservatorytst.ciat.cgiar.org', 
+      'www.fao.org', 
+      'oeactest.ciat.cgiar.org',
+      'www.worldbank.org',
+      'fpma.fao.org',
+      'www.fao.org',
+      'comtradeplus.un.org',
+      'www.magyp.gob.ar',
+      'www.mercadocentral.gob.ar',
+      'sisdep.conab.gov.br',
+      'observatorioagro.gob.bo',
+      'www.odepa.gob.cl',
+      'fedearroz.s3.amazonaws.com',
+      'www.dane.gov.co',
+      'fpma.review.fao.org',
+      'www.ministeriodegobierno.gob.ec',
+      'www.mag.gob.sv',
+      'precios.maga.gob.gt',
+      'www.bch.hn',
+      'th.bing.com',
+      'www.inide.gob.ni',
+      'www.oneplanetnetwork.org',
+      'www.mag.gob.sv',
+      'www.mioa.org',
+      'www.thaitapiocastarch.org',
+    ]
   } 
 }
 
